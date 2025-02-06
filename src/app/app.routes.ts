74998ac9b4f8b2e94cc9344594960f3997cdf2e2
Jatumongkon <jatu023464@gmail.com>
2025-02-06 15:12:24 +0700

    6/2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { SkillComponent } from './skill/skill.component';
 
 const routes: Routes = [
   { path: '', component :HomeComponent},
   { path: 'Project', component : ProjectPageComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'Skill', component : SkillComponent},
+  // {path: '/', redirectTo: 'home', pathMatch: 'full' },
 
 
 ];
